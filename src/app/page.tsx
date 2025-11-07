1fa8ac578e6296d67cba3749df99b8bc191ca665
Alireza Khodabakhshi <cyborgslark@gmail.com>
2025-11-07 20:32:53 +0330

    chore(call): update secondary phone number to 09122568011

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
     "https://maps.google.com/maps?q=35.608100,51.308674&ll=35.608100,51.308674&z=16";
   const phoneNumbers = [
     { label: "09122995956", href: "tel:+989122995956" },
-    { label: "09197916676", href: "tel:+989197916676" },
+    { label: "09122568011", href: "tel:+989122568011" },
   ];
   return (
     <div dir="rtl" className="font-sans bg-background">
